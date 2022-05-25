e81a7ee5fcfe1702256c5f080fe7814d5dc3f808
Ernie Francis <Devin_98@msn.com>
2022-05-25 20:03:08 +0000

    finish react-on-rails starter

--- client/bundles/HelloWorld/components/HelloWorld.tsx
+++ client/bundles/HelloWorld/components/HelloWorld.tsx
@@ -1,3 +1,4 @@
+import { css } from "@emotion/css";
 import * as React from "react";
 import { useState, FunctionComponent } from "react";
 // @ts-ignore
@@ -13,21 +14,23 @@ const HelloWorld: FunctionComponent<Props> = (props: Props) => {
   const [name, setName] = useState(props.name);
 
   return (
-    <div>
-      <h3>Hello, {name}!</h3>
-      <hr />
-      <form>
-        <label className={style.bright} htmlFor="name">
-          Say hello to:
-          <input
-            id="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-      </form>
-    </div>
+      <div>
+        <h3 className={css`
+          color: red;
+        `}>Hello, {name}!</h3>
+        <hr />
+        <form>
+          <label className={style.bright} htmlFor="name">
+            Say hello to:
+            <input
+              id="name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </form>
+      </div>
   );
 };
 
